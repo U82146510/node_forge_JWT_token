40f213e42a240f623d0963802ca5916ebe2319a5
U82146510 <U82146510@protonmail.com>
2025-05-04 21:52:34 +0100

    adding reading and writing data to a file

--- app.ts
+++ app.ts
@@ -1,4 +1,11 @@
 import fs from 'fs';
+import { fileURLToPath } from 'url';
+import path from 'path';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+const input_payload:string = fs.readFileSync("payload.txt","utf-8");
 
 function base64(input:unknown):string|undefined{
     try {
@@ -25,12 +32,12 @@ const payload={
 };
 
 
-const get_token = ():string|undefined=>{
+const get_token = ():void=>{
     try {
         const encode_head = base64(head);
         const encode_payload = base64(payload);
         const token = `${encode_head}.${encode_payload}.`;
-        return token;
+        fs.writeFileSync('output_forge_token.txt',token);
     } catch (error) {
         console.error(error);
     }
